d4df5947c0e63a960c4ee8ae6c95ba0662ea1763
Jana Klemp <janaGit@users.noreply.github.com>
2016-05-18 15:17:51 +0200

    bug fixed in app.js: two getMetadata methods image_Gallery: clicking on an image fixes the metadata table content to that picture, a next click on the same picture unfixes it, click does not lead to href="#" anymore

--- app/ImageGallery/image_Gallery.component.ts
+++ app/ImageGallery/image_Gallery.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Renderer} from 'angular2/core';
 import {ImageService}     from './../services/image.service';
 import {OnMouseOverImageDirective}     from './../directives/onMouseOverImage.directive';
 import {ExifToolService}  from './../services/exifTool.service';
@@ -11,12 +11,12 @@ import {ExifToolService}  from './../services/exifTool.service';
 export class ImageGallery implements OnInit {
     private _errorMessage_imageService: string;
     errorMessage_exifToolService: string;
-
+    private _imageNameClicked: string;
     public imageNames_edited: string[];
     public imgDir_edited: string;
     metadata = {};
     metadata_keys = [];
-    constructor(private _imageService: ImageService, private _exifToolService: ExifToolService) { }
+    constructor(private _imageService: ImageService, private _exifToolService: ExifToolService, private _renderer: Renderer) { }
     ngOnInit() {
         this._imageService.getImageNames_edited().subscribe(
             images => { this.imageNames_edited = images },
@@ -24,14 +24,34 @@ export class ImageGallery implements OnInit {
         );
         this.imgDir_edited = this._imageService.imageDir_edited;
     }
-    onMouseOverImage(imgName) {
-        this.getMetadata(imgName);
+    onMouseOverImage(event) {
+        if (event.event === 'mouseOver') {
+            if (typeof this._imageNameClicked === 'undefined') {
+                this.getMetadata(event.img_name);
+            }
+        }
+        if (event.event === 'mouseClicked') {
+            if (this._imageNameClicked !== event.img_name) {
+                this.getMetadata(event.img_name);
+                this._imageNameClicked = event.img_name;
+                this._renderer.setElementClass(event.element, 'clicked', true);
+            } else {
+                this._imageNameClicked = undefined;
+                this._renderer.setElementClass(event.element, 'clicked', false);
+            }
+        }
     }
     getMetadata(imageName: string) {
         this._exifToolService.getMetadata_edited(imageName).subscribe(
             data => { this.metadata = data; this.metadata_keys = Object.keys(data); },
             error => this.errorMessage_exifToolService = <any>error
         );
+    }
+    imageClicked(imgName: string) {
+        if (this._imageNameClicked === imgName) {
+            return true;
+        }
+        return false;
     }
 }
 
