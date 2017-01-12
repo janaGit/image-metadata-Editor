1c2927127d98e8d561bdea3ab375e44b7cecc663
janaGit <janaGit@users.noreply.github.com>
2017-01-13 00:29:05 +0100

    bootstrap and routing added, app.component: header added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { DropdownModule } from 'ng2-bootstrap';
+
+import { routerConfig } from './app.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+   RouterModule.forRoot(routerConfig, { useHash: true }),
+   DropdownModule.forRoot()
+   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
