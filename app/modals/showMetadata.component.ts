ddfce46fb0589312c4f27e743552c3b86c4d8f71
janaGit <janaGit@users.noreply.github.com>
2017-01-23 22:06:31 +0100

    showMetadata.component: code nicer, comments added

--- app/modals/showMetadata.component.ts
+++ app/modals/showMetadata.component.ts
@@ -1,57 +1,100 @@
-import { Component, Input, Output, ChangeDetectorRef, Inject, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output,  EventEmitter } from '@angular/core';
 import { SimpleChange } from '@angular/core';
 import { ExifToolService } from './../services/exifTool.service';
 
+/**
+* Different values for the css display variable.
+*/
+type css_display = 'block' | 'none'
+/**
+ * This class provides a modal that shows a table with the metadata
+ * of the actual selected image. It is used by the editor view at the 
+ * start page.
+ */
 @Component({
     selector: 'showMetadata',
     templateUrl: 'showMetadata.component.html',
     styleUrls: ['showMetadata.component.css']
 })
-
-export class ShowMetadataComponent {
-    _display_css = 'none';
-    metadata = {};
-    metadata_keys = [];
-    _display: boolean;
+export class ShowMetadataComponent implements OnInit{
+    /**
+     * Actual value of css:display 
+     */
+    private _display_css: css_display = 'none';
+    /**
+     * Actual status of the modal
+     * 
+     * shown: true 
+     * 
+     * not shown: false
+     */
+    private _display: boolean = false;
+    /**
+     * This variable stores the metadata of an image.
+     */
+    private metadata = {};
+    /**
+     * This variable stores the keys of the metadata.
+     */
+    private metadata_keys = [];
+    /**
+     * The status if the modal is visible of hidden can be 
+     * externally defined and changed.
+     */
     @Input() get display() {
         return this._display;
     }
 
+    /**
+     * Event emitter that informs other components about the
+     * actual status (shown/hidden) of the modal.
+     */
     @Output() displayChange = new EventEmitter<boolean>();
 
-    set display(_display) {
-        this._display = _display;
-        this.displayChange.emit(this._display);
-        this.display_Show_Hide();
-    }
     constructor(private _exifToolService: ExifToolService) { }
-    toogle_Show_Hide() {
-        if (this._display_css !== 'none') {
-            this._display_css = 'none';
-            this.display = false;
-        } else {
-            this.showMetadata();
-            this.display = true;
-        }
+    
+    ngOnInit(){
+        // To get sure that _display and _display_css are synchonized. 
+        this.display=this._display;
     }
-    display_Show_Hide() {
+    /**
+     * This method sets the value for the visibility of the modal.
+     * 
+     * If the modal should be shown, then the metadata of the actual image 
+     * are requested and bind to the local metadata variable.
+     * 
+     * true: visible
+     * 
+     * false: hidden
+     */
+    set display(display:boolean) {
+        //actualize _display variable
+        this._display = display;
+        this.displayChange.emit(this._display);
+        
         if (this._display == false) {
             this._display_css = 'none';
         } else {
             this.showMetadata();
         }
     }
+  
+  /**
+   * This method executes a request for getting the metadata 
+   * of the actual image by using the exifToolService.
+   * If the request was successfull, then the modal is shown.
+   * If not, then an alert-window with an error message is displayed. 
+   */
     showMetadata() {
-        let self=this;
         this._exifToolService.requestMetadata().then(
-            function () {
-                let __metadata = self._exifToolService.metadata;
+            () => {
+                let __metadata = this._exifToolService.metadata;
                 if (__metadata) {
-                    self.metadata = __metadata;
-                    self.metadata_keys = Object.keys(self.metadata);
-                    self._display_css = 'block';
+                    this.metadata = __metadata;
+                    this.metadata_keys = Object.keys(this.metadata);
+                    this._display_css = 'block';
                 } else {
-                    alert(self._exifToolService.errorMessage);
+                    alert(this._exifToolService.errorMessage);
                 }
             });
     }
