15e76d2d73b3dea2aec591f1b4b78cee0e9111e4
janaGit <janaGit@users.noreply.github.com>
2019-03-15 22:26:59 +0100

    filt-tab: now metadata of all images are deletable and all images are moveable to the image gallery at once

---
+++ utilities/constants.ts
@@ -0,0 +1,4 @@
+export const IMAGE_EDITED = "edited_";
+export const METADATA_DELETED = "editedx_";
+export const imagePrefixes = [IMAGE_EDITED, METADATA_DELETED];
+export const FILE_NAMES_TO_IGNORE = ["selectAll_Images.png"];
