857edba6f4866a7671f90da7219dd35abbc3217d
janaGit <janaGit@users.noreply.github.com>
2020-07-10 19:55:29 +0200

    Bug-fix: tooltips work again

--- src/app/directives/ime-tooltip.directive.ts
+++ src/app/directives/ime-tooltip.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, Input, ElementRef, Renderer2, OnInit, HostBinding, ViewContainerRef } from '@angular/core';
 import { TooltipDirective, TooltipConfig } from 'ngx-bootstrap/tooltip';
 import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader';
+import { PositioningService } from 'ngx-bootstrap/positioning';
 
 @Directive({
   selector: '[imeTooltip]'
@@ -11,13 +12,13 @@ export class ImeTooltipDirecive implements OnInit {
   @Input() tooltipPlacement: 'top' | 'bottom' | 'left' | 'right' = 'top';
   @HostBinding('attr.tooltip') bootstrapTooltip;
 
-  constructor(private _viewContainerRef: ViewContainerRef, private _elementRef: ElementRef, private renderer2: Renderer2, private _renderer: Renderer2, private cis: ComponentLoaderFactory, private config: TooltipConfig) {
+  constructor(private _viewContainerRef: ViewContainerRef, private _elementRef: ElementRef, private _renderer: Renderer2, private cis: ComponentLoaderFactory, private _config: TooltipConfig,private _positioningService: PositioningService) {
 
   }
 
   ngOnInit() {
     const element = this._elementRef;
-    this.bootstrapTooltip = new TooltipDirective(this._viewContainerRef, this.cis, null, this._elementRef, this._renderer, null);
+    this.bootstrapTooltip = new TooltipDirective(this._viewContainerRef, this.cis, this._config, this._elementRef, this._renderer, this._positioningService);
     this.bootstrapTooltip.tooltip = this.imeTooltip;
     this.bootstrapTooltip.placement = this.tooltipPlacement;
     this.bootstrapTooltip.ngOnInit();
