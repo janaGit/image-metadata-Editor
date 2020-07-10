3d77b08aefb52413c83697f5c294c5e71f05e777
janaGit <janaGit@users.noreply.github.com>
2020-07-10 19:27:00 +0200

    bug-fix: dropdown is supported again (change language)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -59,6 +59,7 @@ import  './font-awesome';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot()
