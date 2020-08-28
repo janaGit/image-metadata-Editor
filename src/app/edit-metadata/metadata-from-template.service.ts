b9933771eb539eccf7499235edae8af5d5f0ee4d
janaGit <janaGit@users.noreply.github.com>
2020-08-28 19:56:58 +0200

    MatadataFromTemplateService: Template Name ist stored now in the service

--- src/app/edit-metadata/metadata-from-template.service.ts
+++ src/app/edit-metadata/metadata-from-template.service.ts
@@ -6,85 +6,102 @@ import { AppTemplate } from 'app/types/app-template.interface';
 import { TemplateExistingMetadataType } from 'app/types/template-existing-metadata.type';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MetadataFromTemplateService {
 
-  private _editMetadata: MetadataFromMetadataTab;
+    private _templateName: string;
 
-  private __editMetadata: BehaviorSubject<MetadataFromMetadataTab> = new BehaviorSubject<MetadataFromMetadataTab>(null);
+    private __templateName: BehaviorSubject<string> = new BehaviorSubject<string>(null);
 
-  public editMetadata$ = this.__editMetadata.asObservable();
-
-
-  private _categories: string[];
-
-  private __categories: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(["Animal","Feciduous Forest", "Buildings", "Open Fields", "See", "Test","Test2"]);
-
-  public categories$ = this.__categories.asObservable();
+    public templateName$ = this.__templateName.asObservable();
 
 
-  private _existingMetadata: TemplateExistingMetadataType;
+    private _editMetadata: MetadataFromMetadataTab;
 
-  private __existingMetadata: BehaviorSubject<TemplateExistingMetadataType> = new BehaviorSubject<TemplateExistingMetadataType>(null);
+    private __editMetadata: BehaviorSubject<MetadataFromMetadataTab> = new BehaviorSubject<MetadataFromMetadataTab>(null);
 
-  public existingMetadata$ = this.__existingMetadata.asObservable();
-
-
-
-  private _location: MetadataFromLocationTab;
-
-  private __location: BehaviorSubject<MetadataFromLocationTab> = new BehaviorSubject<MetadataFromLocationTab>(null);
-
-  public location$ = this.__categories.asObservable();
+    public editMetadata$ = this.__editMetadata.asObservable();
 
 
-  
+    private _categories: string[];
 
-  constructor() {
-  }
+    private __categories: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(["Animal", "Feciduous Forest", "Buildings", "Open Fields", "See", "Test", "Test2"]);
 
-  setTemplate(template: AppTemplate) {
-    this.updateCategories(template.categoryTab);
-    this.updateEditMetadata(template.metadataTab);
-    this.updateExistingMetadata(template.existingMetadataTab);
-    this.updateLocation(template.locationTab);
-}
+    public categories$ = this.__categories.asObservable();
 
 
-  get editMetadata() {
-      return this._editMetadata;
-  }
+    private _existingMetadata: TemplateExistingMetadataType;
 
-  get categories() {
-      return this._categories;
-  }
+    private __existingMetadata: BehaviorSubject<TemplateExistingMetadataType> = new BehaviorSubject<TemplateExistingMetadataType>(null);
 
-  get existingMetadata() {
-    return this._existingMetadata;
-}
+    public existingMetadata$ = this.__existingMetadata.asObservable();
 
-  get location() {
-      return this._location;
-  }
 
-  updateEditMetadata(metadata) {
-      this._editMetadata = metadata;
-      this.__editMetadata.next(metadata);
-  }
 
-  updateCategories(categories) {
-      this._categories = categories;
-      this.__categories.next(categories);
-  }
+    private _location: MetadataFromLocationTab;
 
-  updateExistingMetadata(existingMetadata: TemplateExistingMetadataType) {
-    this._existingMetadata = existingMetadata;
-    this.__existingMetadata.next(existingMetadata);
-}
+    private __location: BehaviorSubject<MetadataFromLocationTab> = new BehaviorSubject<MetadataFromLocationTab>(null);
 
-  updateLocation(location) {
-      this._location = location;
-      this.__location.next(location);
-  }
+    public location$ = this.__categories.asObservable();
+
+
+
+
+    constructor() {
+    }
+
+    setTemplate(template: AppTemplate) {
+        this.updateTemplateName(template.name);
+        this.updateCategories(template.categoryTab);
+        this.updateEditMetadata(template.metadataTab);
+        this.updateExistingMetadata(template.existingMetadataTab);
+        this.updateLocation(template.locationTab);
+    }
+
+    get templateName() {
+        return this._templateName;
+    }
+
+    get editMetadata() {
+        return this._editMetadata;
+    }
+
+    get categories() {
+        return this._categories;
+    }
+
+    get existingMetadata() {
+        return this._existingMetadata;
+    }
+
+    get location() {
+        return this._location;
+    }
+
+
+    updateTemplateName(templateName: string) {
+        this._templateName = templateName;
+        this.__templateName.next(templateName);
+    }
+
+    updateEditMetadata(metadata) {
+        this._editMetadata = metadata;
+        this.__editMetadata.next(metadata);
+    }
+
+    updateCategories(categories) {
+        this._categories = categories;
+        this.__categories.next(categories);
+    }
+
+    updateExistingMetadata(existingMetadata: TemplateExistingMetadataType) {
+        this._existingMetadata = existingMetadata;
+        this.__existingMetadata.next(existingMetadata);
+    }
+
+    updateLocation(location) {
+        this._location = location;
+        this.__location.next(location);
+    }
 }
